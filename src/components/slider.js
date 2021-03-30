dbeba0ef73000eb0a76639bbc4dcbdd4986558ec
seva <sevavac@gmail.com>
2021-03-30 22:07:44 +0300

    slider

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,21 +1,38 @@
-import React from "react";
-import { sliderComponents } from "./sliderComponents";
-import { HiArrowNarrowLeft,HiArrowNarrowRight } from "react-icons/hi";
-import '../styles/app.css';
+import React, {useState} from "react";
+import {sliderComponents} from "./sliderComponents";
+import {HiArrowNarrowLeft, HiArrowNarrowRight} from "react-icons/hi";
+import "../styles/app.css";
 
-export default class Slider extends React.Component {
-  constructor() {
-    super();
-  }
-  render() {
+const Slider = ({slides}) => {
+
+    const [current, setCurrent] = useState(0);
+    const length = slides.length;
+    const nextSlide = () => {
+
+        setCurrent(current === length - 1 ? 0 : current + 1);
+
+    };
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1);
+    };
+
+
     return (
-      <section className='sliderr'>
-      <HiArrowNarrowLeft className='left-arrow'/>
-      <HiArrowNarrowRight className='right-arrow'/>
-        {sliderComponents.map((slide)  => {
-          return <img src={slide.image} className='slider-image' alt="Some pick." />;
-        })}
-      </section>
+
+        <section className="sliderr">
+            <HiArrowNarrowLeft className="left-arrow" onClick={prevSlide}/>
+            <HiArrowNarrowRight className="right-arrow" onClick={nextSlide}/>
+            {sliderComponents.map((slide, index) => {
+                return (
+                    <div
+                        key={index}>
+                        {index === current && (
+                            <img src={slide.image} alt='travel image' className='image'/>
+                        )}
+                    </div>
+                );
+            })}
+        </section>
     );
-  }
-}
+};
+export default Slider;
